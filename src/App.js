6090960947e8ebbff97a4d8a0b0c0b7fcff6426f
shubham hirap <shirap07@gmail.com>
2020-09-20 16:23:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import About from "./components/About";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+import Interest from "./components/Interest";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="page-top">
+      <Navbar />
+      <div className="container-fluid p-0">
+        <About />
+        <Experience />
+        <Education />
+        <Skills />
+        <Interest />
+      </div>
     </div>
   );
 }
